09bd974e76da1af1fb799c1222b7d793a3e0e5eb
benrathi <benrathi@umich.edu>
2017-04-15 14:53:27 -0400

    emojis

--- emojiAnalysis.py
+++ emojiAnalysis.py
@@ -1,29 +1,18 @@
 import csv
 
-def readCSV ():
-	with open('coors.csv', mode='r') as infile:
-	    reader = csv.reader(infile)
-	    with open('coors_new.csv', mode='w') as outfile:
-	        writer = csv.writer(outfile)
-	        mydict = {rows[0]:rows[1] for rows in reader}
-
 def main():
 	print "hello"
 
-	reader = csv.DictReader(open('EmojiSentiment.csv'))
+	reader = csv.reader(open('EmojiSentiment.csv'))
 
 	print reader
 
-	result = []
-	
-	result = list(reader)
+	emojiDict = dict()
 
+	for row in reader:
+		emojiDict[row[0]] = row[1]
 
-	# with open('EmojiSentiment.csv', mode='r') as infile:
-	#     reader = csv.reader(infile)
-	#     with open('coors_new.csv', mode='w') as outfile:
-	#         writer = csv.writer(outfile)
-	#         mydict = {rows[0]:rows[6] for rows in reader}
+	print emojiDict
 
 
 main()
